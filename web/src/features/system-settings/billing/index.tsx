456778124cceca5aab9d46bfc37b33129b05880a
linbiqiu <741671416@qq.com>
2026-07-30 22:02:04 +0800

    fix(web): add personal funding groups default

--- web/src/features/system-settings/billing/index.tsx
+++ web/src/features/system-settings/billing/index.tsx
@@ -58,6 +58,7 @@ const defaultBillingSettings: BillingSettings = {
   AutoGroups: '',
   DefaultUseAutoGroup: false,
   'group_ratio_setting.group_special_usable_group': '{}',
+  'billing_group_setting.personal_funding_groups': '[]',
   PayAddress: '',
   EpayId: '',
   EpayKey: '',
